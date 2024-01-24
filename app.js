3b3d364f7fd27f7a8b942bbb20f62c95a0c0f5d3
brandonkhoza <8brandonkhoza@gmail.com>
2024-01-24 22:43:02 +0200

    Added body on the blogs

--- app.js
+++ app.js
@@ -7,19 +7,24 @@ app.use(cors())
 app.get('/', (req, res) => {
     var data = [{
         "id": "1",
-        "title": "Book Review"
+        "title": "Book Review",
+        "body": "My Review 1"
     },
     {
         "id": "2",
-        "title": "Game Review"
+        "title": "Game Review",
+        "body": "My Review 2"
     },
     {
         "id": "3",
-        "title": "Show Review"
+        "title": "Show Review",
+        "body": "My Review 3",
+        
     },
     {
         "id": "4",
-        "title": "Bible Review"
+        "title": "Bible Review",
+        "body": "My Review 4"
     }];
     res.json(data);
 });
